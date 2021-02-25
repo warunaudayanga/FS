a81f23bbf3c789c07939c0afa989e0147ee741c7
Waruna Udayanga <warunaudayangalakruwan@gmail.com>
2021-02-25 13:19:23 +0530

    5

--- src/main/java/com/wx/jdbc/DataSource.java
+++ src/main/java/com/wx/jdbc/DataSource.java
@@ -10,13 +10,13 @@ public class DataSource {
     static {
         ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
 
-        ds.setUrl("jdbc:mysql://localhost/pos");
-        ds.setUsername("root");
-        ds.setPassword("toor");
+//        ds.setUrl("jdbc:mysql://localhost/pos");
+//        ds.setUsername("root");
+//        ds.setPassword("toor");
 
-//        ds.setUrl("jdbc:mysql://cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/tltpuweeovcae4h0");
-//        ds.setUsername("xnuviaajp26iu5pg");
-//        ds.setPassword("wzpkbp5rkwfw0yfe");
+        ds.setUrl("jdbc:mysql://cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/tltpuweeovcae4h0");
+        ds.setUsername("xnuviaajp26iu5pg");
+        ds.setPassword("wzpkbp5rkwfw0yfe");
 
         ds.setMinIdle(5);
         ds.setMaxIdle(10);
@@ -25,20 +25,6 @@ public class DataSource {
 
     public static Connection getConnection() throws SQLException {
         return ds.getConnection();
-    }
-
-    public static ResultSet readData(String sql) {
-        try (Connection con = DataSource.getConnection();
-             PreparedStatement pst = con.prepareStatement( sql );
-             ResultSet rs = pst.executeQuery();) {
-
-            return rs;
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-
-            return null;
-        }
     }
 
     public static boolean writeData(String sql) {
